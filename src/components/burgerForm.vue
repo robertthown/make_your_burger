5782b1820029d2b203d1869c4ce00982b34b789b
robertthown <robertomello404@gmail.com>
2023-02-14 21:34:17 -0300

    puchamos os dados do banco para serem apresentados nos selects, utilizamos v-for para isso

--- src/components/burgerForm.vue
+++ src/components/burgerForm.vue
@@ -11,31 +11,26 @@
                     <label for="pao">Escolha o pão</label>
                     <select name="pao" id="pao" v-model="pao">
                         <option value="">Selecione o seu pão</option>
-                        <option value="">Integral</option>
+                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                            {{ pao.tipo }}
+                        </option>
                     </select>
                 </div>
                 <div class="input-container">
                     <label for="carne">Escolha a carne do seu Burger:</label>
                     <select name="carne" id="carne" v-model="carne">
                         <option value="">Selecione o tipo de carne</option>
-                        <option value="maminha">Maminha</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                            {{ carne.tipo }}
+                        </option>
                     </select>
                 </div>
                 <div id="opcionais-container" class="input-container">
                     <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                        <span>Salame</span>
+                    <div class="checkbox-container" v-for="opcional in opcionais" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
                     </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                        <span>Salame</span>
-                    </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                        <span>Salame</span>
-                    </div>
-
                 </div>
                 <div class="input-container">
                     <input type="submit" class="submit-btn" value="Criar meu Burger!">
@@ -47,7 +42,35 @@
 
 <script>
     export default{
-        name: 'burgerForm'
+        name: 'burgerForm',
+        data(){
+            return {
+                paes: null,
+                carnes: null,
+                opcionais: null,
+                nome: null, 
+                pao: null,
+                carne: null,
+                opcionais: [],
+                status: "Solicitado",
+                msg: null,
+            }
+        },
+        methods: {
+            async getIngredientes(){
+                
+                const req = await fetch("http://localhost:3000/ingredientes");
+                const data = await req.json();
+
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionais = data.opcionais;
+
+            }
+        },
+        mounted(){
+            this.getIngredientes()
+        }
     }
 </script>
 
@@ -93,7 +116,7 @@
 
     }
 
-    .checkbox-container span
+    .checkbox-container span,
     .checkbox-container input {
         width: auto;
     }
@@ -102,5 +125,21 @@
         margin-left: 6px;
         font-weight: bold;
     }
-    
+
+    .submit-btn{
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover{
+        background: transparent;
+        color: #222;
+    }
 </style>
